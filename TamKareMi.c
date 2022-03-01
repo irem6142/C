4eb7f6c98976864ed80e5f40a646e3c898607d78
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-03-01 20:27:44 +0300

    Tam Kare Mi ?

---
+++ TamKareMi.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+   int a,b=0,c;
+	printf("Sayi giriniz :");
+	scanf("%d",&a);
+	c=sqrt(a);
+	b=c*c;
+	if(b==a)
+	{
+		printf("Tam karedir.");
+	}
+	else
+	{
+	   printf("Tam kare degildir.");
+	}
+
+	
+	
+	
+}
