903d90f0e6ef32b4f3c5d4b09e4ce32d4b35398c
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-03-26 06:37:43 +0300

    Mükemmel Sayı

--- MukemmelSayi.c
+++ MukemmelSayi.c
@@ -6,17 +6,16 @@ int main()
 	printf("Sayi giriniz:");
 	scanf("%d",&a);
 	for(i=1;i<a;i++)
-    {
-    if(a%i==0)
-	{
-		top+=i;
-		}	
+        {
+            if(a%i==0)
+	  {
+	   top+=i;
+	  }	
 	}	
    
 	if(top==a)
-	{
-		printf("Mukemmel sayidir");
-	}
+       printf("Mukemmel sayidir");
+	
 	else
 	printf("Mukemmel sayi degildir.");
 	
