eb2d8d9041ee43e99272d1794c25dc3bd3b0771c
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-06-29 07:37:49 +0300

    Harshad (Niven)

---
+++ Niven.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+	//Rakamlari toplamina tam olarak bolunen sayilara "HARSHAD (NIVEN) SAYILARI" denir.
+	int a,b,i,gecici;
+	int t=0;
+	printf("Sayi :");
+	scanf("%d",&a);
+	gecici=a;
+    while(gecici>0)
+    {
+    	
+    	b=gecici%10;
+        gecici/=10;
+    	t+=b;
+	}
+ 
+	if(a%t==0)
+	{
+		printf("HARSHAD (NIVEN)  sayisidir.");
+	}
+	else  if(a%t!=0)
+	{
+		printf("HARSHAD (NIVEN)  sayisi degildir.");
+	}
+	
+	
+	
+	
+	
+	
+	
+}
