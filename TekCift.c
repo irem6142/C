c7f1881c854cd9d4c23185a0ffe5b4d6dd85a166
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-07-04 20:27:47 +0300

    Bir sayı tek mi çift mi?

---
+++ TekCift.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	printf("Sayi:");
+	int a;
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("Girdiginiz sayi cifttir.");
+	}
+	else
+	{
+		printf("Girdiginiz sayi tektir.");
+	}
+}
