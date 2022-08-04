73f57073ece5cc104a2416efae9340a0e76bf7ba
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-08-04 15:53:43 +0300

    Yaş hesaplama

---
+++ age.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Kac yilinda dogdunuz?:");
+	int i,j;
+	scanf("%d",&i);
+	j=age(i);
+	printf("Yasiniz: %d",j);
+    
+	
+	
+}
+
+	int age(int year)
+	{
+		return 2022-year;
+	
+	}
