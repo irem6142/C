29d2e41d78a5fdbc584d93607f2de9f69acbcb37
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-12-11 23:35:52 +0300

    file.cpp

--- file.cpp
+++ file.cpp
@@ -1,3 +1,4 @@
+
 /*Dosya islemleri*/
 #include <iostream>
 /*Dosya kutuphanesi*/
@@ -15,6 +16,49 @@ int main()
 	
 	/*Dosya kapatma*/
 	file.close();
-	cout<<"Basarili";
+
+
+	/*ifstream ile dosya okuyoruz*/
+	ifstream okumaDosyasi;
+	okumaDosyasi.open("file2.txt");
+	/*Satir satir okuma yapacagiz.*/
+	string satir;
+	/*Tek bir satırı okumak icin*/
+	getline(okumaDosyasi,satir);
+	cout<<satir;
+	/*Butun hepsini okumak icin*/
+	while(getline(okumaDosyasi,satir)){
+		cout<<satir<<endl;
+	}
+	
+	
+    /*kelime kelime okuma yapacagiz.*/
+  
+ifstream okumaDosyasi2("file2.txt");
+	  string kelime;
+    okumaDosyasi2>>kelime;
+    cout<<kelime;
+    /*Butun hepsini okumak icin*/
+    while(okumaDosyasi2>>kelime)
+    {
+    	cout<<kelime<<endl;
+	}
+	/* Ya da boyle de yazılabilir ,butun hepsini okumak icin*/
+
+    while(!okumaDosyasi2.eof())
+    {
+    	okumaDosyasi2>>kelime;
+    	cout<<kelime<<endl;
+	}
+	
+    
+    
+	
+	
+	
+       okumaDosyasi.close();
+       okumaDosyasi2.close();
+	
+	
 	
 }
